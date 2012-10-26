13e7cd5ceac744d56bb9610be5f0245ce8c3b40c
Oliver Charles <oliver.g.charles@googlemail.com>
2012-10-26 15:15:30 -0700

    musicbrainz-server: New cookbook to provision musicbrainz-server instances
    This commit introduces the 'musicbrainz-server' cookbook, which provides recipes
    for provisioning musicbrainz-server instances. There are two recipes:
    
    * musicbrainz-server::install - this recipe "installs" musicbrainz-server, by
      performing a Git clone and installing all Perl dependencies from my PPA.
    
    * musicbrainz-server::production - this recipe extends the ::install recipe by
      configuring nginx as a reverse proxy to a daemontools-backed FastCGI socket.
    
    There is a templated DBDefs.pm which uses attributes to determine how to expand
    this template.

---
+++ musicbrainz-server/recipes/production.rb
@@ -0,0 +1,46 @@
+include_recipe "daemontools"
+include_recipe "musicbrainz-server::install"
+include_recipe "nginx"
+
+package "libcatalyst-plugin-autorestart-perl"
+package "libcatalyst-plugin-errorcatcher-perl"
+package "libfcgi-procmanager-perl"
+package "libjavascript-closure-perl"
+
+service "svscan" do
+  action :start
+  provider Chef::Provider::Service::Upstart
+end
+
+daemontools_service "musicbrainz-server" do
+  directory "/home/musicbrainz/musicbrainz-server/admin/nginx/service/"
+  template false
+  action [:enable,:start]
+  subscribes :restart, "git[/home/musicbrainz/musicbrainz-server]" 
+  subscribes :restart, "template[/home/musicbrainz/musicbrainz-server/lib/DBDefs.pm]"
+end
+
+link "/etc/service/musicbrainz-server/mb_server" do
+  to "/home/musicbrainz/musicbrainz-server"
+  owner "musicbrainz"
+end
+
+script "compile_resources" do
+  user "musicbrainz"
+  interpreter "bash"
+  cwd "/home/musicbrainz/musicbrainz-server"
+  code "./script/compile_resources.pl"
+  subscribes :run, "git[/home/musicbrainz/musicbrainz-server]"
+end
+
+template "/etc/nginx/sites-available/musicbrainz" do
+  source "nginx.conf.erb"
+  user "root"
+  variables :server_name => node['musicbrainz-server']['hostname'];
+end
+
+nginx_site "musicbrainz" do
+  action :nothing
+  subscribes :enable, "template[/etc/nginx/sites-available/musicbrainz]";
+end
+
