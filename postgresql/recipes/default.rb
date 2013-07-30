fa8bcf995114226d1fc005904a6d84b73eda3baa
Robert Kaye <rob@musicbrainz.org>
2013-07-31 01:26:32 +0200

    Add a pg_tune.py script and needed startup scripts for auto tuning PG

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -17,6 +17,43 @@ cookbook_file "/etc/postgresql/9.1/main/pg_hba.conf" do
   owner "postgres"
 end
 
+cookbook_file "/etc/postgresql/9.1/main/postgresql.conf.in" do
+  source "postgresql.conf.in"
+  owner "postgres"
+  group "postgres"
+  mode "0644"
+end
+
+cookbook_file "/etc/init.d/postgresql-config" do
+  source "postgresql-config"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+link "/etc/rc2.d/S18postgresql-config" do
+  to "/etc/init.d/postgresql-config"
+end
+
+link "/etc/rc3.d/S18postgresql-config" do
+  to "/etc/init.d/postgresql-config"
+end
+
+file "/etc/postgresql/9.1/main/postgresql.conf" do
+  action :delete
+end
+
+cookbook_file "/usr/local/bin/tune_pg.py" do
+  source "tune_pg.py"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+service "postgresql-config" do
+  action [ :start ]
+end
+
 service "postgresql" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start, :reload ]
