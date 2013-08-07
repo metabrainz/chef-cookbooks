10d8fbc1f02bcbb702a3a863b2b0e7f5218f7d73
Robert Kaye <rob@musicbrainz.org>
2013-08-07 14:31:18 +0200

    Remove comments from postgres config file and move the pg_tune stuff to virtual-machine

--- virtual-machine/recipes/default.rb
+++ virtual-machine/recipes/default.rb
@@ -43,3 +43,45 @@ cookbook_file "/home/vm/bin/reindex" do
   owner "vm"
   mode "0755"
 end
+
+cookbook_file "/etc/postgresql/9.1/main/postgresql.conf.in" do
+  source "postgresql.conf.in"
+  owner "postgres"
+  group "postgres"
+  mode "0644"
+end
+
+cookbook_file "/etc/init.d/postgresql-config" do
+  source "postgresql-config"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+link "/etc/rc2.d/S18postgresql-config" do
+  to "/etc/init.d/postgresql-config"
+end
+
+link "/etc/rc3.d/S18postgresql-config" do
+  to "/etc/init.d/postgresql-config"
+end
+
+file "/etc/postgresql/9.1/main/postgresql.conf" do
+  action :delete
+end
+
+cookbook_file "/usr/local/bin/tune_pg.py" do
+  source "tune_pg.py"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+service "postgresql-config" do
+  action [ :start ]
+end
+
+service "postgresql" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start, :reload ]
+end
