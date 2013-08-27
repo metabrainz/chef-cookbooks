04ff0844aa78582d5d15fdd22abe8333b7c67748
Oliver Charles <ollie@ocharles.org.uk>
2013-08-27 11:44:42 +0100

    musicbrainz-server::logster: New recipe
    Ships a summary of the nginx access logs to a graphite server

---
+++ musicbrainz-server/recipes/logster.rb
@@ -0,0 +1,17 @@
+include_recipe "logster"
+
+package "git"
+git "/home/musicbrainz/logster" do
+    repository "git://github.com/metabrainz/logster.git"
+    revision "master"
+    action :sync
+    user "musicbrainz"
+end
+
+logster_log "/var/log/nginx/001-musicbrainz.access.log" do
+    prefix "#{node['hostname']}.musicbrainz_server"
+    output "graphite"
+    graphite_host node['graphite']['host']
+    parser "musicbrainz.logster.NginxStatus.NginxStatus"
+    python_path ["/home/musicbrainz/logster"]
+end
