29956999f4f072bb2b82a9695d4079f6c46af437
Oliver Charles <ollie@ocharles.org.uk>
2013-09-03 15:42:48 +0100

    monitoring::rabbitmq: New recipe to monitor RabbitMQ

---
+++ monitoring/recipes/rabbitmq.rb
@@ -0,0 +1,29 @@
+user "monitoring"
+
+include_recipe "apt"
+apt_repository "musicbrainz" do
+  uri "http://ppa.launchpad.net/oliver-charles/musicbrainz/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E4EB3B02925D4F66"
+end
+
+package "gamekeeper"
+
+rabbitmq_user node['monitoring']['rabbitmq']['user'] do
+  action :add
+  password node['monitoring']['rabbitmq']['password']
+end
+
+rabbitmq_user node['monitoring']['rabbitmq']['user'] do
+  vhost node['caa-indexer']['rabbitmq']['vhost']
+  action :set_permissions
+  permissions "'' '' .*"
+end
+
+cron "rabbitmq"
+  mailto "root"
+  user "monitoring"
+  command "gamekeeper measure --uri=http://#{node['monitoring']['rabbitmq']['user']}:#{node['monitoring']['rabbitmq']['password']}@#{node['monitoring']['rabbitmq']['host']}:#{node['monitoring']['rabbitmq']['port']}/ --sink=Graphite,#{node['monitoring']['graphite']['host']},#{node['monitoring']['graphite']['port']}
+end
