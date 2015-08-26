26ecbfd9b3b4a4f7c337a8cff3c9b24911675202
Michael Wiencek <mwtuea@gmail.com>
2015-08-26 15:37:50 -0500

    Revert "Use the same username that's currently on pino"
    This reverts commit 638a8bdffde1677048e963ed68ed943113cb9be5.
    
    It doesn't really matter, but the previous naming was more consistent with
    the other CAA cookbooks.

--- caa-redirect/recipes/default.rb
+++ caa-redirect/recipes/default.rb
@@ -7,23 +7,23 @@ package "python-werkzeug"
 
 service_name = node['caa-redirect'][:service]
 
-user "coverart_redirect" do
+user "caaredirect" do
   action :create
-  home "/home/coverart_redirect"
+  home "/home/caaredirect"
   shell "/bin/bash"
   supports :manage_home => true
 end
 
 package "git"
-git "/home/coverart_redirect/#{service_name}" do
+git "/home/caaredirect/#{service_name}" do
   repository "git://github.com/metabrainz/coverart_redirect.git"
   revision node['caa-redirect'][:revision]
   action :sync
-  user "coverart_redirect"
+  user "caaredirect"
 end
 
-template "/home/coverart_redirect/#{service_name}/coverart_redirect.conf" do
-  owner "coverart_redirect"
+template "/home/caaredirect/#{service_name}/coverart_redirect.conf" do
+  owner "caaredirect"
   mode "644"
   variables :config => node['caa-redirect']
 end
@@ -35,12 +35,12 @@ service "svscan" do
 end
 
 daemontools_service "caa-redirect" do
-  directory "/home/coverart_redirect/svc-#{service_name}"
+  directory "/home/caaredirect/svc-#{service_name}"
   template "coverart_redirect"
   log true
   action [:enable, :start]
 end
 
-link "/home/coverart_redirect/svc-#{service_name}/coverart_redirect" do
-  to "/home/coverart_redirect/#{service_name}"
+link "/home/caaredirect/svc-#{service_name}/coverart_redirect" do
+  to "/home/caaredirect/#{service_name}"
 end
