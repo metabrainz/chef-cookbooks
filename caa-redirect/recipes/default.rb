ee116c03e2e7e6a8465261a8daa303c35f6458d1
Michael Wiencek <mwtuea@gmail.com>
2015-08-27 00:02:25 -0500

    Can't run the caa-redirect recipe twice on the same node
    So set up production and staging services in the same recipe.

--- caa-redirect/recipes/default.rb
+++ caa-redirect/recipes/default.rb
@@ -1,11 +1,10 @@
 include_recipe "apt"
+include_recipe "daemontools"
 
 package "python-cherrypy3"
 package "python-psycopg2"
 package "python-sqlalchemy"
 package "python-werkzeug"
-
-service_name = node['caa-redirect'][:service]
 
 user "caaredirect" do
   action :create
@@ -14,33 +13,39 @@ user "caaredirect" do
   supports :manage_home => true
 end
 
-package "git"
-git "/home/caaredirect/#{service_name}" do
-  repository "git://github.com/metabrainz/coverart_redirect.git"
-  revision node['caa-redirect'][:revision]
-  action :sync
-  user "caaredirect"
-end
-
-template "/home/caaredirect/#{service_name}/coverart_redirect.conf" do
-  owner "caaredirect"
-  mode "644"
-  variables :config => node['caa-redirect']
-end
-
-include_recipe "daemontools"
 service "svscan" do
   action :start
   provider Chef::Provider::Service::Upstart
 end
 
-daemontools_service "caa-redirect" do
-  directory "/home/caaredirect/svc-#{service_name}"
-  template "coverart_redirect"
-  log true
-  action [:enable, :start]
-end
+['production', 'staging'].each do |server|
+  service_name = "coverart_redirect-#{server}"
 
-link "/home/caaredirect/svc-#{service_name}/coverart_redirect" do
-  to "/home/caaredirect/#{service_name}"
+  package "git"
+  git "/home/caaredirect/#{service_name}" do
+    repository "git://github.com/metabrainz/coverart_redirect.git"
+    revision node['caa-redirect'][server.to_sym][:revision]
+    action :sync
+    user "caaredirect"
+  end
+
+  template "/home/caaredirect/#{service_name}/coverart_redirect.conf" do
+    owner "caaredirect"
+    mode "644"
+    variables(
+      :config => node['caa-redirect'],
+      :server => server
+    )
+  end
+
+  daemontools_service "#{service_name}" do
+    directory "/home/caaredirect/svc-#{service_name}"
+    template "coverart_redirect"
+    log true
+    action [:enable, :start]
+  end
+
+  link "/home/caaredirect/svc-#{service_name}/coverart_redirect" do
+    to "/home/caaredirect/#{service_name}"
+  end
 end
