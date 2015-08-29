8a373b227d4e6783e78e983254ea55fbccdf4c74
Michael Wiencek <mwtuea@gmail.com>
2015-08-28 20:13:50 -0500

    Start redis with upstart

--- redis/recipes/default.rb
+++ redis/recipes/default.rb
@@ -1,13 +1,7 @@
-include_recipe "apt"
-include_recipe "daemontools"
-
 package "redis-server"
 
 template "/etc/redis/redis.conf"
 
-daemontools_service "redis-server" do
-  directory "/var/lib/redis/redis-server"
-  template "redis-server"
+service "redis-server" do
   action [:enable, :start]
-  log true
 end
