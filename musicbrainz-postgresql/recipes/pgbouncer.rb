8c29f486f34f687badd25c7e6e953c1436febb77
Michael Wiencek <mwtuea@gmail.com>
2016-05-15 19:37:36 -0500

    Set START=0 in /etc/default/pgbouncer since we're using daemontools

--- musicbrainz-postgresql/recipes/pgbouncer.rb
+++ musicbrainz-postgresql/recipes/pgbouncer.rb
@@ -6,6 +6,12 @@ package "pgbouncer"
 template "/etc/pgbouncer/userlist.txt"
 template "/etc/pgbouncer/pgbouncer.ini"
 
+execute "Disable pgbouncer init script" do
+  command "/bin/sed s/START=1/START=0/ -i /etc/default/pgbouncer"
+  not_if "grep 'START=1' /etc/default/pgbouncer"
+  only_if { platform_family?("debian") }
+end
+
 daemontools_service "pgbouncer" do
   directory "/etc/service/pgbouncer"
   template "pgbouncer"
