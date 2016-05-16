f962faced03216e9e359e290c12b5333f81922d0
Michael Wiencek <mwtuea@gmail.com>
2016-05-15 19:30:39 -0500

    Manage pgbouncer with daemontools

--- musicbrainz-postgresql/recipes/pgbouncer.rb
+++ musicbrainz-postgresql/recipes/pgbouncer.rb
@@ -1,17 +1,14 @@
 include_recipe "apt"
+include_recipe "daemontools"
 
 package "pgbouncer"
 
 template "/etc/pgbouncer/userlist.txt"
 template "/etc/pgbouncer/pgbouncer.ini"
 
-execute "Enable pgbouncer" do
-  command "/bin/sed s/START=0/START=1/ -i /etc/default/pgbouncer"
-  not_if "grep 'START=0' /etc/default/pgbouncer"
-  only_if { platform_family?("debian") }
-end
-
-service "pgbouncer" do
-  supports restart: true
-  action [ :enable, :start ]
+daemontools_service "pgbouncer" do
+  directory "/etc/service/pgbouncer"
+  template "pgbouncer"
+  action [:enable, :up]
+  log true
 end
